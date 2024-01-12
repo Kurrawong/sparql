95098f6b8df007d70692f84d964119a5e3cde069
Edmond Chuc <edmond@kurrawong.ai>
2024-01-12 12:57:11 +1000

    Move the SPARQL grammar into its own file

--- sparql/parser.py
+++ sparql/parser.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 from lark import Lark
 
-from sparql.grammar import grammar
+grammar_path = current_dir = Path(__file__).parent / "grammar.lark"
 
-sparql_parser = Lark(grammar, start="query_unit")
-sparql_update_parser = Lark(grammar, start="update_unit")
+with open(grammar_path, "r", encoding="utf-8") as file:
+    grammar = file.read()
+    sparql_parser = Lark(grammar, start="query_unit")
+    sparql_update_parser = Lark(grammar, start="update_unit")
